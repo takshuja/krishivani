e11059d2983c0805cc12fd2ef4c973e166c5dbc5
takshuja <takshuja@protonmail.com>
2024-12-07 20:16:19 +0530

    Fixed a minor bug.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -62,7 +62,7 @@ export default {
         async logout() {
             await this.store.logout();
             // useRouter().clearRoutes();
-            useRouter().push('/');
+            this.$router.push('/');
         }
     }
 }
